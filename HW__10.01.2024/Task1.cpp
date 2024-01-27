d96f5b1be43e9b7211ffce2ba7c424493b0632e1
“CraftyVlad“ <vladsemboss@gmail.com>
2024-01-27 11:20:36 +0200

    e

--- HW__10.01.2024/Task1.cpp
+++ HW__10.01.2024/Task1.cpp
@@ -1,37 +1,6 @@
-﻿#include <iostream>
-#include <fstream>
-#include <string>
-
-using namespace std;
-
-int main()
-{
-    char ch;
-    cout << "Enter a character to search for: ";
-    cin >> ch;
-
-    string filename = "file1.txt";
-    ifstream file(filename);
-
-    if (!file.is_open())
-    {
-        cout << "Could not open file: " << filename << endl;
-        return 0;
-    }
-
-    int count = 0;
-    string word;
-    while (file >> word)
-    {
-        if (word[0] == ch)
-        {
-            count++;
-        }
-    }
-
-    cout << "Number of words starting with '" << ch << "': " << count << endl;
-
-    file.close();
-
-    return 0;
-}
+﻿//#define MIN(a,b) ((a)<(b)?(a):(b))
+//#define MAX(a,b) ((a)>(b)?(a):(b))
+//#define SQUARE(x) ((x)*(x))
+//#define POWER(x,n) pow((x),(n))
+//#define EVEN(x) ((x)
+//#define ODD(x) ((x)
---
+++ HW__10.01.2024/function.h
@@ -0,0 +1,14 @@
+#ifdef INTEGER
+void FillArray(int arr[], int size);
+void ShowArray(int arr[], int size);
+#endif
+
+#ifdef DOUBLE
+void FillArray(double arr[], int size);
+void ShowArray(double arr[], int size);
+#endif
+
+#ifdef CHAR
+void FillArray(char arr[], int size);
+void ShowArray(char arr[], int size);
+#endif
