4ef98b5a550aa73037662d013345c7dcb0073e92
“CraftyVlad“ <vladsemboss@gmail.com>
2024-02-21 14:12:52 +0200

    made both tasks

--- HW__10.01.2024/Task1.cpp
+++ HW__10.01.2024/Task1.cpp
@@ -1,67 +1,74 @@
-﻿//#include <iostream>
-//#include <algorithm>
-//#include <vector>
-//using namespace std;
-//
-////  максимуму
-//template <typename T>
-//T findMax(const vector<T>& arr) {
-//    return *max_element(arr.begin(), arr.end());
-//}
-//
-//// пошук мінімуму
-//template <typename T>
-//T findMin(const vector<T>& arr) {
-//    return *min_element(arr.begin(), arr.end());
-//}
-//
-//// сортування масиву
-//template <typename T>
-//void sortArray(vector<T>& arr) {
-//    sort(arr.begin(), arr.end());
-//}
-//
-//// двійкий пошук в масиві
-//template <typename T>
-//int binarySearch(const vector<T>& arr, T key) {
-//    return binary_search(arr.begin(), arr.end(), key);
-//}
-//
-//// функція для заміни елемента масиву на передане значення
-//template <typename T>
-//void replaceElement(vector<T>& arr, T oldValue, T newValue) {
-//    replace(arr.begin(), arr.end(), oldValue, newValue);
-//}
-//
-//int main() {
-//    vector<int> arr = { 5, 2, 8, 1, 9, 4 };
-//
-//    cout << "Max: " << findMax(arr) << endl;
-//    cout << "Min: " << findMin(arr) << endl;
-//
-//    sortArray(arr);
-//    cout << "Sorted array: ";
-//    for (const auto& elem : arr) {
-//        cout << elem << " ";
-//    }
-//    cout << endl;
-//
-//    int key = 5;
-//    if (binarySearch(arr, key)) {
-//        cout << key << " found in array" << endl;
-//    }
-//    else {
-//        cout << key << " not found in array" << endl;
-//    }
-//
-//    int oldValue = 4;
-//    int newValue = 10;
-//    replaceElement(arr, oldValue, newValue);
-//    cout << "Array after changes: ";
-//    for (const auto& elem : arr) {
-//        cout << elem << " ";
-//    }
-//    cout << endl;
-//
-//    return 0;
-//}
+﻿#include <iostream>
+using namespace std;
+
+template <class T>
+class Node {
+public:
+    T data;
+    Node* next;
+
+    Node(T data) {
+        this->data = data;
+        this->next = nullptr;
+    }
+};
+
+template <class T>
+class Stack {
+private:
+    Node<T>* top;
+    int size;
+
+public:
+    Stack() {
+        top = nullptr;
+        size = 0;
+    }
+
+    void push(T data) {
+        Node<T>* newNode = new Node<T>(data);
+        newNode->next = top;
+        top = newNode;
+        size++;
+    }
+
+    void pop() {
+        if (top == nullptr) {
+            cout << "Stack is empty." << endl;
+            return;
+        }
+        Node<T>* temp = top;
+        top = top->next;
+        delete temp;
+        size--;
+    }
+
+    T peek() {
+        if (top == nullptr) {
+            cout << "Stack is empty." << endl;
+            return T();
+        }
+        return top->data;
+    }
+
+    bool isEmpty() {
+        return size == 0;
+    }
+};
+
+int main() {
+    Stack<int> stack;
+
+    stack.push(1);
+
+
+    cout << "Top element: " << stack.peek() << endl;
+
+    stack.pop();
+    cout << "Top element after pop: " << stack.peek() << endl;
+
+    stack.push(2);
+    cout << "Top element after push: " << stack.peek() << endl;
+
+    return 0;
+}
--- HW__10.01.2024/Task2.cpp
+++ HW__10.01.2024/Task2.cpp
@@ -1,107 +1,97 @@
-﻿#include <iostream>
-#include <vector>
-#include <cstdlib>
-#include <ctime>
-#include <algorithm>
-using namespace std;
-
-template <typename T>
-class Matrix {
-private:
-    vector<vector<T>> data;
-    int rows;
-    int cols;
-
-public:
-    Matrix(int rows, int cols) : rows(rows), cols(cols) {
-        data.resize(rows);
-        for (int i = 0; i < rows; i++) {
-            data[i].resize(cols);
-        }
-    }
-
-    void fillWithUserInput() {
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                cout << "Enter the [" << i << "]" << "[" << j << "] element: ";
-                cin >> data[i][j];
-            }
-        }
-    }
-
-    void printMatrix() {
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                cout << data[i][j] << " ";
-            }
-            cout << "\n";
-        }
-        cout << "\n";
-    }
-
-    Matrix operator+(const Matrix& right) {
-        if (rows != right.rows || cols != right.cols) {
-            cerr << "Matrix dimensions don't match for addition!" << endl;
-            return Matrix(0, 0);
-        }
-
-        Matrix result(rows, cols);
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                result.data[i][j] = data[i][j] + right.data[i][j];
-            }
-        }
-        return result;
-    }
-
-    Matrix operator-(const Matrix& right) {
-        if (rows != right.rows || cols != right.cols) {
-            cerr << "Matrix dimensions don't match for subtraction!" << endl;
-            return Matrix(0, 0);
-        }
-
-        Matrix result(rows, cols);
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                result.data[i][j] = data[i][j] - right.data[i][j];
-            }
-        }
-        return result;
-    }
-
-    Matrix operator*(const Matrix& right) {
-        if (cols != right.rows) {
-            cerr << "Matrix dimensions are not compatible for multiplication/division!" << endl;
-            return Matrix(0, 0);
-        }
-
-        Matrix result(rows, right.cols);
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < right.cols; j++) {
-                result.data[i][j] = 0;
-                for (int k = 0; k < cols; k++) {
-                    result.data[i][j] += data[i][k] * right.data[k][j];
-                }
-            }
-        }
-        return result;
-    }
-
-};
-
-int main() {
-
-    srand(static_cast<unsigned>(time(nullptr)));
-
-    Matrix<int> mat1(2, 3);
-    mat1.fillWithUserInput();
-    cout << "Matrix 1:" << endl;
-    mat1.printMatrix();
-
-    Matrix<int> mat2(2, 3);
-    mat2.fillWithUserInput();
-    cout << "Matrix 2:" << endl;
-    mat2.printMatrix();
-
-    return 0;
-}
+﻿//#include <iostream>
+//using namespace std;
+//
+//template <class T>
+//class Node {
+//public:
+//    T data;
+//    Node* next;
+//
+//    Node(T data) {
+//        this->data = data;
+//        this->next = nullptr;
+//    }
+//};
+//
+//template <class T>
+//class LinkedList {
+//private:
+//    Node<T>* head;
+//    int size;
+//
+//public:
+//    LinkedList() {
+//        head = nullptr;
+//        size = 0;
+//    }
+//
+//    void push(T data) {
+//        Node<T>* newNode = new Node<T>(data);
+//        newNode->next = head;
+//        head = newNode;
+//        size++;
+//    }
+//
+//    LinkedList<T>* clone() {
+//        LinkedList<T>* clonedList = new LinkedList<T>();
+//        Node<T>* current = head;
+//        while (current != nullptr) {
+//            clonedList->push(current->data);
+//            current = current->next;
+//        }
+//        return clonedList;
+//    }
+//
+//    LinkedList<T>* operator+(const LinkedList<T>& otherList) {
+//        LinkedList<T>* newList = new LinkedList<T>();
+//        Node<T>* current = head;
+//        while (current != nullptr) {
+//            newList->push(current->data);
+//            current = current->next;
+//        }
+//        current = otherList.head;
+//        while (current != nullptr) {
+//            newList->push(current->data);
+//            current = current->next;
+//        }
+//        return newList;
+//    }
+//
+//    LinkedList<T>* operator*(const LinkedList<T>& otherList) {
+//        LinkedList<T>* newList = new LinkedList<T>();
+//        Node<T>* current = head;
+//        while (current != nullptr) {
+//            if (otherList.contains(current->data)) {
+//                newList->push(current->data);
+//            }
+//            current = current->next;
+//        }
+//        return newList;
+//    }
+//
+//    bool contains(T data) {
+//        Node<T>* current = head;
+//        while (current != nullptr) {
+//            if (current->data == data) {
+//                return true;
+//            }
+//            current = current->next;
+//        }
+//        return false;
+//    }
+//};
+//
+//int main() {
+//    LinkedList<int> list1;
+//    list1.push(1);
+//    list1.push(2);
+//
+//    LinkedList<int> list2;
+//    list2.push(2);
+//    list2.push(3);
+//
+//    LinkedList<int>* concatenatedList = list1 + list2;
+//    LinkedList<int>* intersectionList = list1 * list2;
+//
+//    return 0;
+//}
